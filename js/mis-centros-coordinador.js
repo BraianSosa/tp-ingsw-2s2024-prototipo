1a8f42e0196ed0122f31c2621de33ba3918f071c
Braian Sosa <BSosa@gottert.com.ar>
2024-11-24 02:32:49 -0300

    update: visualización de centro desde seleccion en listado + refactor de codigo

--- js/mis-centros-coordinador.js
+++ js/mis-centros-coordinador.js
@@ -1,58 +1,66 @@
-// Datos de los centros de atención
-const centros = [
-    {
-        id: 1,
-        nombre: "Centro Tigre",
-        direccion: "Av. Principal 100, Tigre",
-        coordenadas: [-34.425, -58.579]
-    },
-    {
-        id: 2,
-        nombre: "Centro San Isidro",
-        direccion: "Calle San Martín 200, San Isidro",
-        coordenadas: [-34.472, -58.522]
-    },
-    {
-        id: 3,
-        nombre: "Centro Vicente López",
-        direccion: "Av. Maipú 3000, Vicente López",
-        coordenadas: [-34.526, -58.481]
-    }
-];
-
-const zonaCoords = [
-    [-34.5453075, -58.4932709],
-    [-34.5123563, -58.5227966],
-    [-34.4993419, -58.5507774],
-    [-34.4946732, -58.5564423],
-    [-34.469486, -58.5682869],
-    [-34.4466978, -58.5775566],
-    [-34.4342395, -58.5892296],
-    [-34.4260273, -58.5837364],
-    [-34.4172479, -58.5784149],
-    [-34.4185224, -58.5679436],
-    [-34.4250361, -58.5593605],
-    [-34.4328236, -58.5435677],
-    [-34.444999, -58.5258865],
-    [-34.4488212, -58.5054588],
-    [-34.4615604, -58.499279],
-    [-34.4639664, -58.4941292],
-    [-34.479675, -58.4819412],
-    [-34.4939658, -58.4778214],
-    [-34.5013225, -58.4767914],
-    [-34.5143365, -58.4687233],
-    [-34.5246615, -58.4654617],
-    [-34.530177, -58.4519005],
-    [-34.5347023, -58.4649467],
-    [-34.5451661, -58.4930992]
-];
-
-const zona = {
+const zonaNorte = {
     id: 1,
     nombre: "Zona Norte",
-    coordenadas: zonaCoords,
-    centros: centros
-};
+    centros: [
+      {
+        id: 1,
+        nombre: "Centro San Fernando",
+        coordenadas: [-34.455, -58.548],
+        direccion: "Calle Libertador 1020",
+      },
+      {
+        id: 2,
+        nombre: "Centro Tigre",
+        coordenadas: [-34.426, -58.579],
+        direccion: "Av. Italia 750",
+      },
+      {
+        id: 3,
+        nombre: "Centro Pilar",
+        coordenadas: [-34.458, -58.914],
+        direccion: "Ruta 8 Km 52",
+      },
+    ],
+    poligono: {
+      color: "blue",
+      coordenadas: [
+        [-34.5475698, -58.4922409],
+        [-34.5167411, -58.5173035],
+        [-34.4994833, -58.5512924],
+        [-34.4904287, -58.5729218],
+        [-34.4884478, -58.5969543],
+        [-34.5204185, -58.5904312],
+        [-34.5512458, -58.6058807],
+        [-34.5549217, -58.6244202],
+        [-34.5648176, -58.6288834],
+        [-34.5699064, -58.6542892],
+        [-34.5874322, -58.6793518],
+        [-34.6049543, -58.705101],
+        [-34.627558, -58.7219238],
+        [-34.6312305, -58.7432098],
+        [-34.6350441, -58.7650108],
+        [-34.636174, -58.7842369],
+        [-34.630948, -58.7984848],
+        [-34.6049543, -58.8197708],
+        [-34.5978893, -58.8192558],
+        [-34.5680688, -58.8491249],
+        [-34.5139122, -58.8767624],
+        [-34.4907117, -58.8898087],
+        [-34.4873159, -58.9237976],
+        [-34.453634, -58.942337],
+        [-34.3955786, -59.0254211],
+        [-34.3136663, -58.8705826],
+        [-34.2901265, -58.7809753],
+        [-34.3423022, -58.704071],
+        [-34.4091757, -58.5942078],
+        [-34.4533509, -58.5063171],
+        [-34.486184, -58.4815979],
+        [-34.5325811, -58.4541321],
+        [-34.547287, -58.4953308],
+      ],
+    },
+  };
+  
 
 try {
     const map = L.map('map').setView([-34.472, -58.522], 11);
@@ -64,19 +72,19 @@ try {
     }).addTo(map);
 
     // Marcadores de los centros
-    zona.centros.forEach(centro => {
+    zonaNorte.centros.forEach(centro => {
         L.marker(centro.coordenadas)
             .addTo(map)
-            .bindPopup(`<b>Zona:</b> ${zona.nombre}  <br> <b>Centro:</b> ${centro.id} - ${centro.nombre}<br><b>Dirección:</b> ${centro.direccion}`);
+            .bindPopup(`<b>Zona:</b> ${zonaNorte.nombre}  <br> <b>Centro:</b> ${centro.id} - ${centro.nombre}<br><b>Dirección:</b> ${centro.direccion}`);
     });
 
-    // Polígono para delimitar la zona
-    const poligonoZona = L.polygon(zona.coordenadas, {
-        color: '#3388ff',
-        fillColor: '#3388ff',
+    // Polígono para delimitar la zonaNorte
+    const poligonoZona = L.polygon(zonaNorte.poligono.coordenadas, {
+        color: zonaNorte.poligono.color,
+        fillColor: zonaNorte.poligono.color,
         fillOpacity: 0.3
     }).addTo(map);
-    poligonoZona.bindPopup(`Zona asignada: ${zona.nombre}`);
+    poligonoZona.bindPopup(`Zona asignada: ${zonaNorte.nombre}`);
     
     //throw new Error("Prueba de error");
 
@@ -97,7 +105,7 @@ try {
                 </tr>
             </thead>
             <tbody>
-                ${zona.centros.map(centro => `
+                ${zonaNorte.centros.map(centro => `
                     <tr>
                         <td class="border border-gray-300 px-4 py-2">${centro.id}</td>
                         <td class="border border-gray-300 px-4 py-2">${centro.nombre}</td>
